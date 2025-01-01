a3ab09977545003233c32c872d10e357b0689659
Rafael Akchurin <rafael.akchurin@diladele.com>
2025-01-01 10:56:52 +0100

    adding postgres

--- admin/scripts/05_postgres.sh
+++ admin/scripts/05_postgres.sh
@@ -8,3 +8,16 @@ fi
 
 # install postgres
 apt -y install postgresql postgresql-contrib
+
+# recreate the initial database
+sudo -u postgres psql <<- EOSQL1
+    DROP DATABASE IF EXISTS cloud_swg; 
+    CREATE DATABASE cloud_swg;
+EOSQL1
+
+# recreate user
+sudo -u postgres psql <<- EOSQL2
+    DROP ROLE IF EXISTS cloud_swg;
+    CREATE ROLE cloud_swg LOGIN PASSWORD 'cloud_swg';
+    GRANT ALL PRIVILEGES ON DATABASE cloud_swg TO cloud_swg;
+EOSQL2
