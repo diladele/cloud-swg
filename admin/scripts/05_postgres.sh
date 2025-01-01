3d3fec6522e67a2a0635f04158bff39a761014d0
Rafael Akchurin <rafael.akchurin@diladele.com>
2025-01-01 10:41:12 +0100

    adding postgres

---
+++ admin/scripts/05_postgres.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# all web packages are installed as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# install postgres
+apt -y install postgresql postgresql-contrib
