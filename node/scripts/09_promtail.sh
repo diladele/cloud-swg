8659f79988f0b6dafdb750416d062253c0667005
Rafael Akchurin <rafael.akchurin@diladele.com>
2025-02-07 10:18:09 +0100

    adding promtail service

---
+++ node/scripts/09_promtail.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# all packages are installed as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# install this version of exporter
+VERSION="3.3.2"
+
+# download
+wget https://github.com/grafana/loki/releases/download/v${VERSION}/promtail-linux-amd64.zip
+
+# extract contents and remove original archive
+unzip promtail-linux-amd64.zip && rm promtail-linux-amd64.zip
+
+# move to bin
+mv promtail-linux-amd64 /usr/local/bin/promtail
+
+# return to parent folder
+popd
+
+# and check exporter is installed
+promtail --version
+
+# create systemctl service file
+cat >/etc/systemd/system/promtail.service << EOL
+[Unit]
+Description=Promtail Service
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=proxy
+Group=proxy
+Type=simple
+Restart=on-failure
+RestartSec=5s
+StandardOutput=append:/opt/cloud-swg-node/var/log/promtail.log
+StandardError=append:/opt/cloud-swg-node/var/log/promtail.log
+ExecStart=/usr/local/bin/promtail
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+# reload the systemd, enable the service and check its status
+systemctl daemon-reload
+systemctl enable promtail
+systemctl restart promtail
+
+# good then
+systemctl status promtail
